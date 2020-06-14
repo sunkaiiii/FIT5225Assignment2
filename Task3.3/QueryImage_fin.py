3e776181743a669502fa854701ad7d929a9c0043
孙楷 <704725181@qq.com>
2020-06-14 15:09:47 +1000

    fix: fix error of query image

--- Task3.3/QueryImage_fin.py
+++ Task3.3/QueryImage_fin.py
@@ -1,6 +1,6 @@
-import boto3
 import json
-from boto3.dynamodb.conditions import Attr, And
+import boto3
+from boto3.dynamodb.conditions import Attr
 
 
 def lambda_handler(event, context):
@@ -18,10 +18,12 @@ def lambda_handler(event, context):
         query_parameters = event["queryStringParameters"]  # get the query
         tags = []
         [tags.append(value) for value in query_parameters.values()]  # store all the tags in a list
-        if len(tags) == 1:
-            filter_expression = Attr("tag").contains(tags[0])  # if only one searching tags
-        else:
-            filter_expression = And(*[(Attr("tag").contains(value)) for value in tags])
+        filter_expression = None
+        for value in tags:  # make the filter condition
+            if filter_expression is None:
+                filter_expression = Attr("tag").contains(value)
+            else:
+                filter_expression = filter_expression & Attr("tag").contains(value)
         dynamodb = boto3.resource("dynamodb")
         table = dynamodb.Table("FIT5225Assignment2")
         response = table.scan(
@@ -38,3 +40,7 @@ def lambda_handler(event, context):
             "body": json.dumps(result),
             "isBase64Encoded": "false"
         }
+
+
+if __name__ == '__main__':
+    print(lambda_handler(json.load(open("test.json", "rb")), 1))
